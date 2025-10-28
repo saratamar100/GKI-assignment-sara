c7853265920ff7addceb7ff4e065d1397ea5481d
שרה תמר אמיתי <saratama@g.jct.ac.il>
2025-10-28 12:44:01 +0200

    Header component

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header>
+      <Link href="/">Shop</Link>
+      <nav>
+        <ul className={styles["nav-list"]}>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/men">Men</Link>
+          </li>
+          <li>
+            <Link href="/women">Women</Link>
+          </li>
+          <li>
+            <Link href="/jewelry">Jewelry</Link>
+          </li>
+          <li>
+            <Link href="/electronics">Electronics</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact Us</Link>
+          </li>
+        </ul>
+      </nav>
+      <Link href="/cart">Cart</Link>
+    </header>
+  );
+};
+export default Header;
