bf3a544b033fcdd945d330ac626731a541bcb63a
שרה תמר אמיתי <saratama@g.jct.ac.il>
2025-10-28 11:55:44 +0200

    basic fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+import { Product } from "@/types";
+import { useEffect, useState } from "react";
+import { fetchProducts } from "./services/products";
+
 export default function Home() {
-  return <div>HOME</div>;
+  const [products, setProducts] = useState<Product[]>([]);
+  useEffect(() => {
+    fetchProducts().then((data) => setProducts(data));
+  }, []);
+  return <div>{JSON.stringify(products)}</div>;
 }
