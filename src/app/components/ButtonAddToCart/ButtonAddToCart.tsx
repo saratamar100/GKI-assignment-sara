20228a8229de1e5d3a9fe7b81aa5a7d34b0d6935
שרה תמר אמיתי <saratama@g.jct.ac.il>
2025-11-02 14:51:09 +0200

    ButtonAddToCart css

--- src/app/components/ButtonAddToCart/ButtonAddToCart.tsx
+++ src/app/components/ButtonAddToCart/ButtonAddToCart.tsx
@@ -1,9 +1,14 @@
 import { useCartStore } from "@/app/store/cartStore";
 import { Product } from "@/types";
+import styles from "./ButtonAddToCart.module.css";
 
 const ButtonAddToCart = ({ product }: { product: Product }) => {
   const addToCart = useCartStore((state) => state.addToCart);
   const handleAddToCart = () => addToCart(product);
-  return <button onClick={handleAddToCart}>ADD TO CART</button>;
+  return (
+    <button className={styles.button} onClick={handleAddToCart}>
+      ADD TO CART
+    </button>
+  );
 };
 export default ButtonAddToCart;
